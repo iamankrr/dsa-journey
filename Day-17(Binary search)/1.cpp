65f51b8da5610d925c4f953093b6c3ba77ca7bac
iamankrr <amankrr.info@gmail.com>
2026-09-04 19:48:09 +0530

    learn BinarySearch using Two POinter

---
+++ Day-17(Binary search)/1.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int binarySearch(vector<int>arr, int target){
+    int n = arr.size();
+    int st = 0, end = n - 1;
+
+    while(st <= end){
+        int mid = st + (st + end)/2;
+
+        if(target > arr[mid]){
+            st = mid + 1;
+        }
+        else if(target < arr[mid]){
+            end = mid -1;
+        }else{
+            return mid;
+        }
+    }
+    return -1;
+}
+int main(){
+   vector<int>arr = {-1,0,3,4,5,9,12}; // odd size
+    int target = 12;
+
+    vector<int>arr2 = {-1,0,3,5,9,12}; //even size
+
+   cout <<  binarySearch(arr,12);
+   return 0;
+}
